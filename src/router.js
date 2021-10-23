976e4a1a597429171e1033213f7e4f65ac5ff3a1
kibroman <you@example.comkibromanx@gmail.com>
2021-10-22 23:16:33 -0700

    updated

--- src/router.js
+++ src/router.js
@@ -1,13 +1,19 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import ArtistDetail from './pages/artists/ArtistDetail.vue';
+// import ArtistDetail from './pages/artists/ArtistDetail.vue';
 import ArtistsList from './pages/artists/ArtistsList.vue';
-import ArtistRegistation from './pages/artists/ArtistRegistration.vue';
-import ContactArtist from './pages/requests/ContactArtist.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import ArtistRegistration from './pages/artists/ArtistRegistration.vue';
+// import ContactArtist from './pages/requests/ContactArtist.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const ArtistDetail = () => import('./pages/artists/ArtistDetail.vue');
+const ArtistRegistration = () => import('./pages/artists/ArtistRegistration.vue');
+const ContactArtist = () => import('./pages/requests/ContactArtist.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
 
 const router = createRouter({
   history: createWebHistory(),
@@ -22,7 +28,7 @@ const router = createRouter({
         { path: 'contact', component: ContactArtist } 
       ]
     },
-    { path: '/register', component: ArtistRegistation, meta: { requiresAuth: true} },
+    { path: '/register', component: ArtistRegistration, meta: { requiresAuth: true} },
     { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true} },
     { path: '/auth', component: UserAuth, meta: { requiresUnauth: true} },
     { path: '/:notFound(.*)', component: NotFound }
